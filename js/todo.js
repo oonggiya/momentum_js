88eddd4e1299ed607a41779196fb70e9641694ec
jrich <oonggiya@gmail.com>
2022-10-04 19:56:42 +0900

    7.4 todos part1

--- js/todo.js
+++ js/todo.js
@@ -8,10 +8,11 @@ function deleteToDo(event) {
     li.remove();
 }
 
+const TODOS_KEY = "todos";
 const toDos = [];
 
 function saveToDos() {
-    localStorage.setItem("todos", JSON.stringify(toDos));
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function paintToDo(newTodo) {
@@ -39,3 +40,13 @@ function handleToDoSubmit(event) {
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    parsedToDos.forEach((item) =>
+        console.log("this is the turn of", item));
+}
